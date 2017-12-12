48d5600f674fbd2e10e3357d464bc996a348ab5c
240509865e <34485582+240509865e@users.noreply.github.com>
2017-12-12 23:45:15 +0500

    01

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 importScripts("sw-toolbox.js");
 
 toolbox.precache(["index.html","assets/*"]);
 
-toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get("/images/*", toolbox.cacheFirst);
 
-toolbox.router.get('/*', toolbox.networkFirst, {  networkTimeoutSeconds: 5});
+toolbox.router.get("/*", toolbox.networkFirst, {  networkTimeoutSeconds: 5});
